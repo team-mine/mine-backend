22a6a277c18482c252d229912403917f1065d00d
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2024-04-30 00:17:26 +0900

    backend

--- mine/src/main/java/com/example/mine/repository/UserRepository.java
+++ mine/src/main/java/com/example/mine/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.mine.repository;
 import com.example.mine.entity.ScrapEntity;
 import com.example.mine.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUser(String user);
+    @Query("SELECT u FROM UserEntity u WHERE SIZE(u.scraps) > 0")
     Optional<UserEntity> findUserWithNonEmptyScraps();
 }
